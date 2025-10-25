02abda5ab54029223cf9526f492df562d7c63ffe
Tiagoble <ti.abreu.da.rocha@gmail.com>
2025-10-25 13:52:28 -0300

    Operadores lógicos

---
+++ 09-operadores-logicos.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+void main(){
+    int a=1, b=2;
+
+    printf("a = %d, b = %d\n",a,b);
+
+    //menor que
+    printf("a < b : %d\n", a < b);
+
+    //maior que
+    printf("a > b : %d\n", a > b);
+
+    //equivalente
+    printf("a == b : %d\n", a == b);
+
+    //menor ou igual
+    printf("a <= b : %d\n", a <= b);
+
+    //maior ou igual
+    printf("a >= b : %d\n", a >= b);
+
+    //diferente
+    printf("a != b : %d\n", a != b);
+}
