8b04047d06f0d4217d713290effa4648a6efd725
Tiagoble <ti.abreu.da.rocha@gmail.com>
2025-10-27 17:43:49 -0300

    Operadores lógicos AND, OR

--- 09-operadores-logicos.c
+++ 09-operadores-logicos.c
@@ -22,4 +22,32 @@ void main(){
 
     //diferente
     printf("a != b : %d\n", a != b);
+    
+
+    //operador relacional retorna verdadeiro
+    int verdadeiro = 1==1;
+
+    //operador de negação inverte valor
+    int negar = !verdadeiro;
+    printf("Valor: %d\n", verdadeiro);
+    printf("Valor negado: %d\n", negar);
+
+
+    //operadores relacionais com retorno true ou false (!=0, 0)
+    int relacional1 = (5 > 3);
+    int relacional2 = (5 > 4);
+
+    printf("5 > 3? %d\n", relacional1);
+    printf("5 > 4? %d\n", relacional2);
+
+    //operador AND
+    int comparadacao_and = relacional1 && relacional2;
+    printf("%d and %d = %d\n", relacional1, relacional2, comparadacao_and);
+
+    //operador OR
+    int comparadacao_or = (5 > 6) || (6 > 8);
+    printf("(5 > 6) OR (6 > 8): %d\n", comparadacao_or);
+    printf("(5 > 6) OR (8 > 5): %d\n", (5 > 6) || (8 > 5));
+
+    printf("Valor negativo negado: %d\n",!(-8));
 }
