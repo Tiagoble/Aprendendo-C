e3c0c840ab0f92c8912cf493812e2e32a07266d4
Tiagoble <ti.abreu.da.rocha@gmail.com>
2025-10-30 18:11:50 -0300

    Estrutura condicional II

--- 11-est-condicional-simples.c
+++ 11-est-condicional-simples.c
@@ -10,4 +10,17 @@ void main(){
     }else{
         printf("O número %d é ímpar\n", x);
     }
+
+    int y;
+
+    printf("\nDigite um inteiro: ");
+    scanf(" %d", &y);
+
+    if(y < 0){
+        printf("O número é negativo.\n");
+    }else if(y == 0){
+        printf("O número é igual a zero\n");
+    }else{
+        printf("O número é positivo.\n");
+    }
 }
--- main.c
+++ main.c
@@ -1,13 +1,5 @@
 #include<stdio.h>
 
 void main(){
-    int x;
-    printf("Digite um inteiro: ");
-    scanf("%d", &x);
-
-    if(x % 2 == 0){
-        printf("O número %d é par.\n", x);
-    }else{
-        printf("O número %d é ímpar\n", x);
-    }
+    
 }
